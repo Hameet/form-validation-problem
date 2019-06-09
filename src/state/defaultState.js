fb0266f52f8dd3049a1e6ab10b69070f57672d9e
Hameet <hameet.singh@hotmail.com>
2019-06-10 11:32:47 +1200

    Initial commit with Gatsby

--- src/state/defaultState.js
+++
@@ -1,9 +0,0 @@
-export const defaultState = {
-  animals: [],
-  colour: '',
-  email: '',
-  errors: [],
-  password: '',
-  success: false,
-  tigerType: ''
-}
--- src/state/selectors/index.js
+++
@@ -1,5 +0,0 @@
-const getEmail = ({ email }) => email
-const getPassword = ({ password }) => password
-const getAnimals = ({ animals }) => animals
-
-export { getEmail, getAnimals, getPassword }
